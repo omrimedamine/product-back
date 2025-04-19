8a2e26b2a31c7f4c7568645aa9417acc815019c2
Mohammed Amine <mohammedamine.omri@intelcia.com>
2025-04-19 19:41:57 +0100

    init

---
+++ src/main/java/com/example/productback/ProductBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.productback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductBackApplication.class, args);
+    }
+
+}
